b78fdb9abce5d1596fbd817f1cad4392a74be311
jngvarr <jngvarr@gmail.com>
2024-10-22 15:03:11 +0700

    almost work

--- auth-service/src/main/java/ru/jngvarr/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/ru/jngvarr/authservice/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package ru.jngvarr.authservice.config;
 
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,14 +15,8 @@ import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.OrRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import ru.jngvarr.authservice.services.CustomAuthenticationProvider;
-import security.UserDetailsServiceImpl;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Configuration
